4f20d4cc73960f712e18bab3cff2bba38fddcc10
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-10-07 15:37:35 -0700

    Clean up test file

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ var server = require('./server/index').server;
 var db = require('./db/index').db;
 var express = require('express');
 
-var mytest = require('./test');
+//var mytest = require('./test');
 
 var port = 5000;
 
-mytest.test();
 
-// db.open(function (error, mongoclient){
-//   if(error){
-//     throw 'Error opening database';
-//   }
-//   console.log('now listening on ', port);
-//   server.listen(port);
-// })
+db.open(function (error, mongoclient){
+  if(error){
+    throw 'Error opening database';
+  }
+  console.log('now listening on ', port);
+  server.listen(port);
+})
