999789d48a5af6c898b4a2479e2e7d8b60f764f9
Anthony Zavadil <anthony.zavadil@gmail.com>
2014-10-09 12:28:56 -0700

    Environmental variables

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var server = require('./server/index').server;
 var db = require('./db/index').db;
 var express = require('express');
 
-//var mytest = require('./test');
+var mytest = require('./test');
 
 var port = 5000;
 
